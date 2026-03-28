aa90738313c9d1f04ff87787669b9de1cc46946d
AnishBastakoti <anishbastakoti7@gmail.com>
2026-03-29 01:49:06 +0930

    added PWA fetaures

---
+++ templates/js/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'skillpulse-v1';
+const ASSETS = [
+  '/',
+  '/templates/stylesheet/style.css',
+  '/templates/js/main.js'
+];
+
+// Install: Cache essential assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
