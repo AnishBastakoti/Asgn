d0d604017b8a9d30aec474536b2b51af4431adb7
AnishBastakoti <anishbastakoti7@gmail.com>
2026-04-14 20:48:23 +0930

    removed git changes bugs

--- templates/js/service-worker.js
+++ templates/js/service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const CACHE_NAME = 'skillpulse-v1';
 const ASSETS = [
   '/',
@@ -21,27 +20,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-=======
-// const CACHE_NAME = 'skillpulse-v1';
-// const ASSETS = [
-//   '/',
-//   '/templates/stylesheet/style.css',
-//   '/templates/js/main.js'
-// ];
-
-// // Install: Cache essential assets
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-//   );
-// });
-
-// // Fetch: Serve from cache if offline
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
->>>>>>> dc9ff5da2beacc545df23e12bc139397f3583791
